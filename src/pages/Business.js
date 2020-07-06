80bb7131a915caa9fcf6b9bd9c70615488fe861f
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2020-07-06 12:12:37 +0300

    Made pages for 'Legislation' and 'Business' and added necessary vocabularies to them

---
+++ src/pages/Business.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Business = () => (
+    <Layout>
+        <SEO title="Business vocabulary" />
+        <h1>Life Engine vocabulary - Business</h1>
+            <a name={"RegistrationDate"}><h2>RegistrationDate</h2></a>
+            <p>Date of registration of the company</p>
+    </Layout>
+)
+
+export default Business
---
+++ src/pages/Legislation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Legislation = () => (
+    <Layout>
+        <SEO title={"Legislation vocabulary"} />
+        <h1>Life Engine vocabulary - Legislation</h1>
+            <a name={"attribution"}><h2>Attribution</h2></a>
+            <p>List of text to show required credits to data sources</p>
+    </Layout>
+)
+
+export default Legislation
