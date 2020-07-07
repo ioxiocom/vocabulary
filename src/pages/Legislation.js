0266cb37f1f90b6741be2e3a2c399a8a6d2d170f
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2020-07-07 14:51:11 +0300

    Business vocab (#1)
    * Made pages for 'Legislation' and 'Business' and added necessary vocabularies to them
    
    * Added links to pages at 'index.js'
    
    * Some minor fixes to 'Business.js' and 'Legislation.js'
    
    * bug fix

---
+++ src/pages/Legislation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Legislation = () => (
+    <Layout>
+        <SEO title={"Legislation vocabulary"} />
+        <h1>Life Engine vocabulary - Legislation</h1>
+            <a name={"attribution"}><h2>Attribution</h2></a>
+            <p>Text required to give proper attribution to data source</p>
+    </Layout>
+)
+
+export default Legislation
