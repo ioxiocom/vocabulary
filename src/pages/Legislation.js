308bda016528ee4ffc8aff56a38d7ad1688447ad
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2020-07-07 12:57:46 +0300

    Some minor fixes to 'Business.js' and 'Legislation.js'

--- src/pages/Legislation.js
+++ src/pages/Legislation.js
@@ -7,7 +7,7 @@ const Legislation = () => (
         <SEO title={"Legislation vocabulary"} />
         <h1>Life Engine vocabulary - Legislation</h1>
             <a name={"attribution"}><h2>Attribution</h2></a>
-            <p>List of text to show required credits to data sources</p>
+            <p>Text required to give proper attribution to data source</p>
     </Layout>
 )
 
