d2ab2bf263e2a9e219795285c1335d89e851aff0
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2020-08-26 11:50:08 +0300

    Added Company/Shareholders terms to LE vocabulary

--- src/pages/Business.js
+++ src/pages/Business.js
@@ -9,6 +9,12 @@ const Business = () => (
         <h1>Life Engine vocabulary - Business</h1>
             <a name={"registrationDate"}><h2>Registration Date</h2></a>
             <p>Date of registration of the company</p>
+            <a name={"ownership"}><h2>Ownership</h2></a>
+            <p>A state or a fact of possessing something</p>
+            <a name={"totalShares"}><h2>TotalShares</h2></a>
+            <p>A total number of shares issued to a share series</p>
+            <a name={"quantity"}><h2>Quantity</h2></a>
+            <p>Sum of countable or measurable discrete items</p>
     </Layout>
 )
 
--- src/pages/Legislation.js
+++ src/pages/Legislation.js
@@ -8,6 +8,8 @@ const Legislation = () => (
         <h1>Life Engine vocabulary - Legislation</h1>
             <a name={"attribution"}><h2>Attribution</h2></a>
             <p>Text required to give proper attribution to data source</p>
+            <a name={"name"}><h2>Name</h2></a>
+            <p>Legal name of a company or first name and last name of a person.</p>
     </Layout>
 )
 
